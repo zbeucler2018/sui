b340c0cde19ad208b3f341aadd98db2b1a29333f
zbeucler-largo <110923569+zbeucler-largo@users.noreply.github.com>
2023-07-27 14:36:48 +0000

    transpiled basic server with package.json

---
+++ server/app.ts
@@ -0,0 +1,63 @@
+import { WebSocketServer, WebSocket } from 'ws';
+import * as http from 'http';
+import express, { Express } from 'express';
+
+////
+// Type definitions
+////
+
+type DataRequest = {
+	_type: "data" | "command";
+	appName?: string; // is this needed? server already knows appName
+	propsStub?: string; // remove if this isn't needed / figure out what this is for
+	tabName: string;
+	zmqPort: number;
+	zmqCommand: string;
+}
+
+interface Client {
+	websocket: WebSocket;
+	currentTab?: string;
+	zmqsocket?: any;
+}
+
+
+
+const app: Express = express();
+const server: http.Server = http.createServer(app);
+const wss: WebSocketServer = new WebSocket.Server({ server });
+const clients: Map<number, Client> = new Map();
+
+
+const handleRequest = (msg, client) => {
+    console.log(`Got request from ${client.id}, ${JSON.stringify(msg)}`)
+}
+
+
+wss.on('connection', (socket: WebSocket) => {
+
+	// handle new client
+	const clientId = clients.size + 1;
+	const newClient: Client = { websocket: socket };
+	clients.set(clientId, newClient);
+    console.log(`client ${clientId} connected`)
+
+	// handle incomming message
+	socket.on('message', (_msg: string) => {
+		const msg = JSON.parse(_msg);
+		handleRequest(msg, newClient);
+	});
+
+	// handle client leaving
+	socket.on('close', () => {
+		// newClient?.zmqSocket.close();
+		clients.delete(clientId);
+        console.log(`Client ${clientId} disconnected`);
+	});
+});
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+	console.log('Server running');
+});
